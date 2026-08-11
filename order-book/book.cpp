d8d6bc954b6ac3edfa98d131e43ab3050d8b1aa8
Aditya Roy Chowdhury <adiroy2@Adityas-MacBook-Pro.local>
2026-08-11 20:25:52 +0530

    book constructs

--- order-book/book.cpp
+++ order-book/book.cpp
@@ -12,5 +12,23 @@ Book::Book() : buyTree(nullptr), sellTree(nullptr), lowestSell(nullptr),
 
 // destructor ensures garbage collection and freeing of memory
 Book::~Book() {
-    
+    for (auto &[id, order] : orderMap) {
+        delete order;
+    }
+    orderMap.clear();
+
+    for (auto &[limitPrice, limit] : limitBuyMap) {
+        delete limit;
+    }
+    limitBuyMap.clear();
+
+    for (auto &[limitPrice, limit] : limitSellMap) {
+        delete limit;
+    }
+    limitSellMap.clear();
+
+    for(auto &[stopPrice, stopLevel] : stopMap) {
+        delete stopLevel;
+    }
+    stopMap.clear();
 }
--- order-book/book.hpp
+++ order-book/book.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 #include <unordered_map>
 #include <vector>
 #include <random>
@@ -19,9 +20,40 @@ private:
     Limit *highestStopSell;
     Limit *lowestStopBuy;
 
+    std::unordered_map<int, Order*> orderMap;
+    std::unordered_map<int, Limit*> limitBuyMap;
+    std::unordered_map<int, Limit*> limitSellMap;
+    std::unordered_map<int, Limit*> stopMap;
+
+    void addLimit(int limitPrice, bool buyOrSell);
+    void addStop(int stopPrice, bool buyOrSell);
+    Limit* insert(Limit* root, Limit* limit, Limit* parent = nullptr);
+    Limit* insertStop(Limit* root, Limit* limit, Limit* parent = nullptr);
+    void updateBookEdgeInsert(Limit* newLimit);
+    void updateStopBookEdgeInsert(Limit* newStop);
+    void updateBookEdgeRemove(Limit* limit);
+    void updateStopBookEdgeRemove(Limit* limit);
+    void changeBookRoots(Limit* limit);
+    void changeStopBookRoots(Limit* limit);
+    void deleteLimit(Limit* limit);
+    void deleteStopLevel(Limit* limit);
+    void deleteFromOrderMap(int orderId);
+    void deleteFromLimitMap(int limitPrice, bool buyOrSell);
+    void deleteFromStopMap(int stopPrice);
+    int limitOrderAsMarketOrder(int orderId, bool buyOrSell, int shares, int limitPrice);
+    int stopOrderAsMarketOrder(int orderId, bool buyOrSell, int shares, int stopPrice);
+    int existingOrderAsMarketOrder(Order* headOrder, bool buyOrSell);
+    int stopLimitOrderAsLimitOrder(int orderId, bool buyOrSell, int shares, int limitPrice, int stopPrice);
+    void executeStopOrders(bool buyOrSell);
+    void stopLimitOrderToLimitOrder(Order* headOrder, bool buyOrSell);
+    void marketOrderHelper(int orderId, bool buyOrSell, int shares);
+
 public:
     Book (Limit *_buyTree, Limit *_sellTree, Limit *_lowestSell, Limit *_highestBuy
     , Limit *_stopBuyTree, Limit *_stopSellTree, Limit *_highestStopSell, Limit *_lowestStopBuy) 
         : buyTree(_buyTree), sellTree(_sellTree), lowestSell(_lowestSell), highestBuy(_highestBuy), 
         stopBuyTree(_stopBuyTree), stopSellTree(_stopSellTree), highestStopSell(_highestStopSell), lowestStopBuy(_lowestStopBuy) {}
+    
+    int executeOrdersCount = 0;
+    int AVLTreeBalanceCount = 0;
 };
